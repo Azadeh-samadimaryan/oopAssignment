932f3e8693ef724d3203b101a29135fc791c20aa
azadeh.samadi <samadi.azadeh@gmail.com>
2023-09-05 22:06:06 +0200

    todoItem

--- src/main/java/com/workshop/TodoItem.java
+++ src/main/java/com/workshop/TodoItem.java
@@ -3,6 +3,12 @@ package com.workshop;
 public class TodoItem {
     private int id;
     private boolean assigned;
+    private Person assignee;
+
+    public int TodoItem(int id, boolean assigned, Person assignee ){
+        public int getId() {
+            return int id;
+        }
 
 
 }
